42ed4806e606180ab57bf9c357b275cf5435d51f
Christian Kovanen <christian@intor.fi>
2024-08-12 02:04:20 +0300

    Add check-image.sh

---
+++ check-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+p=$(find . -type f \( -name '*.png' -o -name '*.PNG' -o -name '*.jpg' -o -name '*.JPG' -o -name '*.nef' -o -name '*.NEF' \) -printf '%p,')
+c=$(echo "$p" | tr ' ,' '- ' | wc -w)
+echo "Found $c files"
+
+IFS=',' read -r -a a <<< "$p" 
+i=1
+for f in "${a[@]}"
+do
+	echo "Checking $i/$c $f"
+	ffmpeg -nostdin -i "$f" -loglevel error -f null -
+	i=$((i+1)) 
+done
