05afca8fcf67ff50fcf769166d704ce4ece6ac92
Christian Kovanen <christian@intor.fi>
2024-08-17 21:51:19 +0300

    feat: add command line argument for check location

--- check-image/check-image.sh
+++ check-image/check-image.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-p=$(find . -type f \( -name '*.png' -o -name '*.PNG' -o -name '*.jpg' -o -name '*.JPG' -o -name '*.nef' -o -name '*.NEF' \) -printf '%p,')
+if [ -z "$1" ]; then
+	exit 1
+fi
+
+p=$(find "$1" -type f \( -name '*.png' -o -name '*.PNG' -o -name '*.jpg' -o -name '*.JPG' -o -name '*.nef' -o -name '*.NEF' \) -printf '%p,')
 c=$(echo "$p" | tr ' ,' '- ' | wc -w)
 echo "Found $c files"
 
